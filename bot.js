125d2b8745eb36ec325b756be634b06d0bfea004
YOONKIWOONG <realeyed@gmail.com>
2018-05-15 01:46:04 +0900

    서버 시작 시 Console 출력 내용 변경 및 예외 처리 추가

--- bot.js
+++ bot.js
@@ -7,74 +7,101 @@ var db = new sqlite3.Database("./sandboxversion.db");
 
 var cmsUrl = "http://sandbox-cms.kakaopage.com/info";
 var apiUrl = "https://sandbox-api2-page.kakao.com/api/info";
+var exidUrl = "https://sandbox-charlie.kakaopage.com/exid/api/server-info";
+var botStartCheckList = "\n" + "SANDBOX" + "\n";
 
-request(cmsUrl, function(err, res, body) {
+request(cmsUrl, function (err, res, body) {
   if (!err) {
-    var cmsJson = JSON.parse(body);
-    var cmsBranch = cmsJson.git.branch;
-    if (cmsBranch === "develop") {
-      var cmsDevelop = cmsBranch;
-      console.log("CMS : " + apiDevelop);
-    } else if (cmsBranch === "master") {
-      var cmsMaster = cmsBranch;
-      console.log("CMS : " + cmsMaster);
+    if (res.statusCode == 200) {
+      var cmsJson = JSON.parse(body);
+      var cmsBranch = cmsJson.git.branch;
+      if (cmsBranch === "develop") {
+        db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+          "Develop",
+          1
+        ]);
+      } else if (cmsBranch === "master") {
+        db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+          "Master",
+          1
+        ]);
+      } else {
+        var cmsBranchSplit = cmsBranch.split("/");
+        var cmsSandboxVersion = cmsBranchSplit[2] + "_" + cmsBranchSplit[3];
+
+        db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+          cmsSandboxVersion,
+          1
+        ]);
+      }
     } else {
-      var cmsBranchSplit = cmsBranch.split("/");
-      var cmsSandboxVersion = cmsBranchSplit[2] + "_" + cmsBranchSplit[3];
-      console.log("CMS : " + cmsSandboxVersion);
+      console.log("CMS Response Code : " +
+        res.statusCode + "\n")
     }
   }
-  db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
-    cmsSandboxVersion,
-    1
-  ]);
 });
 
-db.get("SELECT module, version FROM sandboxserver WHERE id = 2", function(
-  err,
-  row
-) {
-  console.log(row.module + " : " + row.version);
-});
-
-request(apiUrl, function(err, res, body) {
+request(exidUrl, function (err, res, body) {
   if (!err) {
-    var apiJson = JSON.parse(body);
-    var apiBranch = apiJson.git.branch;
-    if (apiBranch === "develop") {
-      var apiDevelop = apiBranch;
-      console.log("API : " + apiDevelop);
-    } else if (apiBranch === "master") {
-      var apiMaster = apiBranch;
-      console.log("API : " + apiMaster);
+    if (res.statusCode == 200) {
+      console.log(body);
     } else {
-      var apiBranchSplit = apiBranch.split("/");
-      var apiSandboxVersion = apiBranchSplit[2] + "_" + apiBranchSplit[3];
-      console.log("API : " + apiSandboxVersion);
+      console.log("EXID Response Code : " +
+        res.statusCode + "\n")
     }
   }
-  db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
-    apiSandboxVersion,
-    3
-  ]);
+})
+
+request(apiUrl, function (err, res, body) {
+  if (!err) {
+    if (res.statusCode == 200) {
+      var apiJson = JSON.parse(body);
+      var apiBranch = apiJson.git.branch;
+      if (apiBranch === "develop") {
+        db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+          "Develop",
+          3
+        ]);
+      } else if (apiBranch === "master") {
+        db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+          "Master",
+          3
+        ]);
+      } else {
+        var apiBranchSplit = apiBranch.split("/");
+        var apiSandboxVersion = apiBranchSplit[2] + "_" + apiBranchSplit[3];
+
+        db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+          apiSandboxVersion,
+          3
+        ]);
+      }
+    } else {
+      console.log("CMS Response Code : " +
+        res.statusCode + "\n")
+    }
+  }
 });
 
-db.get("SELECT module, version FROM sandboxserver WHERE id = 4", function(
-  err,
-  row
-) {
-  console.log(row.module + " : " + row.version);
-});
+db.each(
+  "SELECT module, version FROM sandboxserver",
+  function (err, row) {
+    botStartCheckList +=
+      row.module + " : " + row.version + "\n";
+  },
+  function () {
+    console.log(botStartCheckList)
+  }
+);
 
 var bot = new slackbots(slacktoken);
 
-bot.on("start", function() {
+bot.on("start", function () {
   console.log("BOT START" + "\n");
 });
 
-bot.on("message", function(data) {
+bot.on("message", function (data) {
   if (data.type === "message") {
-    console.log(data);
     var botId = data.bot_id;
 
     //jenkins_bot : B3SS13WEL
@@ -82,106 +109,111 @@ bot.on("message", function(data) {
     //qa_bot : B7H8GG57X
     if (botId === "B3SS13WEL" && data.attachments) {
       var valueInformation = data.attachments[0].fields[0].value;
+      var valueInformation = valueInformation.toUpperCase();
+      var splitInformation = valueInformation.split("\n");
 
       if (
         valueInformation.includes("SUCCESSFUL") &&
-        valueInformation.includes("nicolas-build-deploy-test-kranes")
+        valueInformation.includes("NICOLAS-BUILD-DEPLOY-TEST-KRANES")
       ) {
-        var valueInformation = valueInformation.toUpperCase();
-        var splitInformation = valueInformation.split("\n");
-        var deployServer = splitInformation[3].slice(12, 19);
-        var deployModule = splitInformation[1].slice(8);
-        var serverVersion = splitInformation[2].slice(10);
+        if (splitInformation[3].includes("SANDBOX") && splitInformation[1].includes("CMS")) {
+          request(cmsUrl, function (err, res, body) {
+            if (!err) {
+              if (res.statusCode == 200) {
+                var cmsJson = JSON.parse(body);
+                var cmsBranch = cmsJson.git.branch;
+                if (cmsBranch === "develop") {
+                  db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+                    "Develop",
+                    1
+                  ]);
+                } else if (cmsBranch === "master") {
+                  db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+                    "Master",
+                    1
+                  ]);
+                } else {
+                  var cmsBranchSplit = cmsBranch.split("/");
+                  var cmsSandboxVersion = cmsBranchSplit[2] + "_" + cmsBranchSplit[3];
 
-        if (deployServer == "SANDBOX" && deployModule == "CMS") {
-          request(cmsUrl, function(err, res, body) {
-            if (!err) {
-              var cmsJson = JSON.parse(body);
-              var cmsBranch = cmsJson.git.branch;
-              if (cmsBranch === "develop") {
-                var cmsDevelop = cmsBranch;
-                console.log("CMS : " + apiDevelop);
-              } else if (cmsBranch === "master") {
-                var cmsMaster = cmsBranch;
-                console.log("CMS : " + cmsMaster);
+                  db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+                    cmsSandboxVersion,
+                    1
+                  ], function () {
+                    bot.postMessageToChannel(
+                      "qa_bot_test",
+                      "배포완료 *CMS* : `" +
+                      apiSandboxVersion +
+                      "`"
+                    );
+                  });
+                }
               } else {
-                var cmsBranchSplit = cmsBranch.split("/");
-                var cmsSandboxVersion =
-                  cmsBranchSplit[2] + "_" + cmsBranchSplit[3];
-                console.log("CMS : " + cmsSandboxVersion);
+                console.log("CMS Response Code : " +
+                  res.statusCode + "\n")
               }
             }
-            db.run(
-              "UPDATE sandboxserver SET version = ? WHERE id = ?",
-              [apiSandboxVersion, 1],
-              function() {
-                bot.postMessageToChannel(
-                  "qa_bot_test",
-                  "배포완료 *" +
-                    deployModule +
-                    "* : `" +
-                    apiSandboxVersion +
-                    "`"
-                );
-              }
-            );
           });
-        } else if (deployServer == "SANDBOX" && deployModule == "API") {
-          request(apiUrl, function(err, res, body) {
+        } else if (splitInformation[3].includes("SANDBOX") && splitInformation[1].includes("API")) {
+          request(apiUrl, function (err, res, body) {
             if (!err) {
-              var apiJson = JSON.parse(body);
-              var apiBranch = apiJson.git.branch;
-              if (apiBranch === "develop") {
-                var apiDevelop = apiBranch;
-                console.log("API : " + apiDevelop);
-              } else if (apiBranch === "master") {
-                var apiMaster = apiBranch;
-                console.log("API : " + apiMaster);
+              if (res.statusCode == 200) {
+                var apiJson = JSON.parse(body);
+                var apiBranch = apiJson.git.branch;
+                if (apiBranch === "develop") {
+                  db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+                    "Develop",
+                    3
+                  ]);
+                } else if (apiBranch === "master") {
+                  db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+                    "Master",
+                    3
+                  ]);
+                } else {
+                  var apiBranchSplit = apiBranch.split("/");
+                  var apiSandboxVersion = apiBranchSplit[2] + "_" + apiBranchSplit[3];
+
+                  db.run("UPDATE sandboxserver SET version = ? WHERE id = ?", [
+                    apiSandboxVersion,
+                    3
+                  ], function () {
+                    bot.postMessageToChannel(
+                      "qa_bot_test",
+                      "배포완료 *API* : `" +
+                      apiSandboxVersion +
+                      "`"
+                    );
+                  });
+                }
               } else {
-                var apiBranchSplit = apiBranch.split("/");
-                var apiSandboxVersion =
-                  apiBranchSplit[2] + "_" + apiBranchSplit[3];
-                console.log("API : " + apiSandboxVersion);
+                console.log("CMS Response Code : " +
+                  res.statusCode + "\n")
               }
             }
-            db.run(
-              "UPDATE sandboxserver SET version = ? WHERE id = ?",
-              [apiSandboxVersion, 3],
-              function() {
-                bot.postMessageToChannel(
-                  "qa_bot_test",
-                  "배포완료 *" +
-                    deployModule +
-                    "* : `" +
-                    apiSandboxVersion +
-                    "`"
-                );
-              }
-            );
           });
         }
       } else if (
         valueInformation.includes("SUCCESSFUL") &&
-        valueInformation.includes("slide_test_krane")
+        valueInformation.includes("SLIDE_TEST_KRANE")
       ) {
-        var valueInformation = valueInformation.toUpperCase();
-        var splitInformation = valueInformation.split("\n");
-        var deployServer = splitInformation[2].slice(10, 16);
-        var slideBranchSplit = splitInformation[2].split("/");
-        var slideSandboxVersion =
-          slideBranchSplit[1] + "_" + slideBranchSplit[2];
-        console.log("SLIDE : " + slideSandboxVersion);
-      }
-      db.run(
-        "UPDATE sandboxserver SET version = ? WHERE id = ?",
-        [slideSandboxVersion, 4],
-        function() {
-          bot.postMessageToChannel(
-            "qa_bot_test",
-            "배포완료 *" + deployModule + "* : `" + apiSandboxVersion + "`"
+        if (splitInformation[2].includes("SANDBOX")) {
+          var slideSandboxVersion =
+            slideBranchSplit[1] + "_" + slideBranchSplit[2];
+
+          db.run(
+            "UPDATE sandboxserver SET version = ? WHERE id = ?", [slideSandboxVersion, 4],
+            function () {
+              bot.postMessageToChannel(
+                "qa_bot_test",
+                "배포완료 *SLIDE* : `" +
+                slideSandboxVersion +
+                "`"
+              );
+            }
           );
         }
-      );
+      }
     }
 
     var checkServerVersion = data.text;
@@ -190,11 +222,11 @@ bot.on("message", function(data) {
       var sandboxVersionList = "*SANDBOX*" + "\n";
       db.each(
         "SELECT module, version FROM sandboxserver",
-        function(err, row) {
+        function (err, row) {
           sandboxVersionList +=
             "*" + row.module + "* : `" + row.version + "`" + "\n";
         },
-        function() {
+        function () {
           bot.postMessageToChannel("qa_bot_test", sandboxVersionList);
         }
       );
