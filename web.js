675b8f3be020bcb08fd8e37c26713e7d319214bc
YOONKIWOONG <realeyed@gmail.com>
2018-08-21 18:41:55 +0900

    web.js 포트 8080으로 연결되지 않게 변경

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get("/", function (req, res) {
     res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(process.env || 8080, function () {
     var host = server.address().address;
     var port = server.address().port;
 
